20dfe52ef3bc2e03d9b2f4d5ca99633c0660eebf
chi-rui <xic768210@yahoo.com.tw>
2014-04-05 21:46:55 +0800

    可以執行尚未排版的程式
    介面元件建立完成
    按扭動作設定完成

--- src/BMI.java
+++ src/BMI.java
@@ -1,20 +1,57 @@
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.*;
+
 
 public class BMI extends JFrame{
+	
+	JTextField jftName = new JTextField(8);
+	JTextField jftAge = new JTextField(8);
+	JTextField jftHeight = new JTextField(8);
+	JTextField jftWeight = new JTextField(8);
+	JTextField jftResult = new JTextField(8);
+	JButton jbtBMI = new JButton("­pºâBMI");
+	
 	public BMI(){
+		
 	setLayout(new FlowLayout(FlowLayout.LEFT, 10, 20));
 	
 	add(new JLabel("Name :"));
-	add(new JTextField(8));
+	add(jftName);
 	add(new JLabel("Age :"));
-	add(new JTextField(8));
-	add(new JLabel("BMI"));
-	add(new JTextField(8));
+	add(jftAge);
+	add(new JLabel("Height :"));
+	add(jftHeight);
+	add(new JLabel("Weight :"));
+	add(jftWeight);
+	add(jbtBMI);
+	add(new JLabel("BMI :"));
+	add(jftResult);
+	
+	jbtBMI.addActionListener(new ButtonListener());
+	}
+		
+	
+	class ButtonListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e){
+			
+			String n = jftName.getText();
+			String a = jftAge.getText();
+			double h = Double.parseDouble(jftHeight.getText());
+			double w = Double.parseDouble(jftWeight.getText());
+			
+			if(h >= 100)
+				h = h/100;
+				
+			double bmi = w / (h * h);
+			jftResult.setText(String.format("Name : " + n +"\nAge : " + a + "\nBMI : %.2f",bmi));
+		}
 	}
 	
 	public static void main(String[] args){
-		BMI frame = new BMI();
+		JFrame frame = new BMI();
 		frame.setTitle("BMI");
 		frame.setSize(200, 200);
 		frame.setLocationRelativeTo(null);
