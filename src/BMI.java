da72d74710d33b3ca608476550ade47ca0f640fa
chi-rui <xic768210@yahoo.com.tw>
2014-04-06 13:46:36 +0800

    使用課本範例計算BMI
    有問題尚未除錯

--- src/BMI.java
+++ src/BMI.java
@@ -47,16 +47,14 @@ public class BMI extends JFrame{
 		public void actionPerformed(ActionEvent e){
 			// get the information users import.
 			String n = jftName.getText();
-			String a = jftAge.getText();
+			int a =  Integer.parseInt(jftHeight.getText());
 			double h = Double.parseDouble(jftHeight.getText());
 			double w = Double.parseDouble(jftWeight.getText());
 			
-			// change centimeters into meters.
-			if(h >= 100)
-				h = h/100;
+			MathBMI ans = new MathBMI(n, a, h, w);
 			
 			// compute BMI.	
-			double bmi = w / (h * h);
+			double bmi = ans.getBMI();
 			// put out the result.
 			jftResult.setText(String.format("Your BMI : %.2f",bmi));
 		}
@@ -72,3 +70,58 @@ public class BMI extends JFrame{
 		frame.setVisible(true);
 	}
 }
+
+class MathBMI{
+	  private String name;
+	  private int age;
+	  private double weight;// in pound
+	  private double height;// in inches
+	  public static final double KILOGRAMS_PER_POUND = 0.45359237;
+	  public static final double METERS_PER_INCH = 0.0254;
+	  
+	  public MathBMI(String name , int age , double weight, double height){
+		this.name = name;
+		this.age  = age;
+		this.weight = weight;
+		this.height = height;
+	  }
+	  
+	  public MathBMI(String name , double weight , double height){
+		this(name , 20 , weight , height);  
+	  }
+	  
+	  public double getBMI(){
+		double bmi = weight * KILOGRAMS_PER_POUND / ((height * METERS_PER_INCH) * (height * METERS_PER_INCH));
+		return Math.round(bmi * 100)/100.0;
+	  }
+	  
+	  public String getStatus(){
+		double bmi = getBMI();
+		if(bmi < 18.5)
+			return "Underweght";
+		else if (bmi<25)
+			return "Normal";
+		else if (bmi < 30)
+			return "Overweight";
+		else
+			return "Obese";
+		
+	  }
+	  
+	  public String getName(){
+		return name;
+	  }
+	  
+	  public int getAge(){
+	   return age;
+	  }
+
+	  public double getWeight(){
+	   return weight;
+	  }
+	  
+	  public double getHeight(){
+	   return height;
+	  }
+	  
+	}
